8abe4aa516943ca5f0f6d8c62a6955a6c51a9805
arjun2690 <arjun26@gmail.com>
2016-02-17 01:04:02 +0530

    player data mocks

--- public/js/app.js
+++ public/js/app.js
@@ -8,4 +8,26 @@ window.onload = function (event) {
 	socket.on('chat message', function(msg){
 	    $('#messages').append($('<li>').text(msg));
 	});
+	$.ajax({
+	  url: "./json/players.json",
+	  method: "get",
+	  dataType: "text",
+	  contentType: "application/json"
+	}).always(function (response) {
+		var list = $.parseJSON(response);
+		var playerDetail= "";
+		$("#list").append(('<span>Name</span>'));
+		$("#list").append(('<span>Base Price</span>'));
+		$("#list").append(('<span>Current Price</span>'));
+		$("#list").append(('<span>Team</span>'));
+		$("#list").append(('<span>Status</span><br>'));
+		$("#list").append(list.map(function(value) {
+			playerDetail = "";
+			for (key in value) {
+		    	playerDetail += ('<span>' + value[key] + '</span>');
+		    }
+		    playerDetail += ('<br>');
+		    return playerDetail;
+		}).join(""));
+	});
 };
