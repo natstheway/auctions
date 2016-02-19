8eb698c160a3f16c7f2abdad08cc73b1d9557e99
Natstheway <natstheway@gmail.com>
2016-02-19 20:47:05 +0530

    Update server.js
    Now, the auction will start after 3 players have joines...
    Can adjust the number of players by changing auction_size.

--- server.js
+++ server.js
@@ -8,9 +8,15 @@ app.use(express.static('public'));
 var playerList = players.getAllPlayers();
 var currentPlayerIndex = 0;
 var timer = null;
+var initial_timer_expiry = 30;
+var extension_timer = 10;
+var client_count = 0;
+var client = [];    // variable to store all the users logged in .. the list is updated when some one is disconnected
+var auction_size = 3;  // number of players to be in the auction group ..can be changed here..
 var setExpiration = function () {
   clearTimeout(timer);
   timer = setTimeout(function () {
+  if(client_count == auction_size) {  // setting timers only when all the participants are in !!
     io.emit("Timer Stop");
     if (playerList[currentPlayerIndex].team) {
        playerList[currentPlayerIndex].status = "sold";
@@ -24,48 +30,70 @@ var setExpiration = function () {
       currentPlayerIndex = currentPlayerIndex + 1;
       io.emit("Current Player", playerList[currentPlayerIndex]);
       io.emit('bid update', playerList[currentPlayerIndex].basePrice);
-      io.emit("Timer Start", 30);
+      io.emit("Timer Start", initial_timer_expiry);
       setExpiration(io);
     } else {
       playerList.splice(0, playerList.length);
       playerList = null;      
     }
-  }, 30 * 1000);  
+  }
+  }, initial_timer_expiry * 1000);  
 };
 var startAuction = function (socket, name) {
-  socket.on('bid message', function(msg){
-    io.emit('bid message', name + ":" + msg);
+    socket.on('bid message', function(msg){
+    if(client_count == auction_size) {    // starting the auction and showing the first player only after everyone joins
+    socket.emit('bid message', name + ":" + msg);
+    socket.broadcast.emit('bid message', name + ":" + msg);
     playerList[currentPlayerIndex].currentPrice = msg;
     playerList[currentPlayerIndex].team = name;
-    io.emit('player update', playerList[currentPlayerIndex]);
-    io.emit('bid update', playerList[currentPlayerIndex].currentPrice + 10);
-    io.emit("Timer Start", 30);
-    setExpiration(io);
+    socket.emit('player update', playerList[currentPlayerIndex]);
+    socket.emit('bid update', playerList[currentPlayerIndex].currentPrice + 10);
+    socket.emit("Timer Start", initial_timer_expiry);
+    socket.broadcast.emit('player update', playerList[currentPlayerIndex]);
+    socket.broadcast.emit('bid update', playerList[currentPlayerIndex].currentPrice + 10);
+    socket.broadcast.emit("Timer Start", initial_timer_expiry);
+    setExpiration(socket);
+  }
   });
   if (!playerList || !playerList.length) {
     playerList = players.getAllPlayers();
     currentPlayerIndex = 0;
   }
-  socket.emit("Current Player", playerList[currentPlayerIndex]);
-  socket.emit("Timer Start", 30);
-  setExpiration(io);
-};
+  
+  if(client_count == auction_size) {
+    socket.emit("Current Player", playerList[currentPlayerIndex]);
+    socket.emit("Timer Start", initial_timer_expiry);
+    socket.broadcast.emit("Current Player", playerList[currentPlayerIndex]);
+    socket.broadcast.emit("Timer Start", initial_timer_expiry);
+    setExpiration(socket);
+  }
+  
+ };
 io.on('connection', function(socket){
   socket.on('Registration', function (name) {
-    io.emit('bid message', name + ' : connected');
+    socket.emit('bid message', name + ' : connected');
+    client.push(name);
+    client_count++;
     socket.on('disconnect', function(){
-      io.emit('bid message', name + ' : disconnected');
+      socket.emit('bid message', name + ' : disconnected');
+      socket.broadcast.emit('bid message', name + ' : disconnected');
+      var delete_index =client.indexOf(name);
+      client.splice(delete_index, 1);
+      client_count--;
     });
     socket.on('start auction', function () {
-      startAuction(socket, name);
-      io.emit('bid message', name + " joined the auction");
+      startAuction(socket, name); // start auction for all here ...
+      socket.emit('bid message', name + " joined the auction");
     });    
   });
-  io.emit("Register", "Please provide your name");
+  socket.emit("Register", "Please provide your name");
 });
 app.get('/', function (req, res) {
   res.sendFile('index.html', { root: path.join(__dirname, '/public') })
 });
+
+
+
 http.listen(8000, function () {
   console.log('Example app listening on port 8000!');
 });
