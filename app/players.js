746c9b796291fd46192f670879e38ad92e98ca02
Natstheway <natstheway@gmail.com>
2016-02-22 01:57:45 +0530

    Update players.js

--- app/players.js
+++ app/players.js
@@ -8,7 +8,7 @@ module.exports = {
         "currentPrice": "",
         "team": "",
         "status": "bidding",
-        "summary": "Lets welcome Thala ! The most successful captain of the Indian cricket and the IPL !!! All yours .. starting with Base Price of 2C !! Adichukonga !!! :P",
+        "summary": "Lets welcome Dhoni ! The most successful captain of the Indian cricket and the IPL !!! All yours .. starting with Base Price of 2C !!",
         "age": 34,
         "country": "India",
         "category": "Wicket Keeper",
@@ -32,7 +32,7 @@ module.exports = {
         "currentPrice": "",
         "team": "",
         "status": "bidding",
-        "summary": "Lets welcome the chinna thalapathy Raina ! The IPL Legend can be all yours now.. starting from the Base Price of 2C !! Its now or never.... Have a go !",
+        "summary": "Lets welcome Mr. Suresh Raina ! The IPL Legend can be all yours now.. starting from the Base Price of 2C !! Its now or never.... Have a go !",
         "age": 27,
         "country": "India",
         "category": "Batsman"
@@ -44,7 +44,7 @@ module.exports = {
         "currentPrice": "",
         "team": "",
         "status": "bidding",
-        "summary": "Lets welcome the most exciting player of the auction .. The man of steel, The Run Machine - Mr Kohli !!! All yours .. starting with Base Price of 2C !! Adichukonga !!!! ",
+        "summary": "Lets welcome the most exciting player of the auction .. The man of steel, The Run Machine - Mr Kohli !!! All yours .. starting with Base Price of 2C !! ",
         "age": 27,
         "country": "India",
         "category": "Batsman"
@@ -56,7 +56,7 @@ module.exports = {
         "currentPrice": "",
         "team": "",
         "status": "bidding",
-        "summary": "Lets welcome the most exciting player of the auction !!! All yours .. starting with Base Price of 2C !! Adichukonga !!! :P",
+        "summary": "Lets welcome the styling leftie, MoS WC 2011 Yuvraj Singh !!! All yours .. starting with Base Price of 2C !!!",
         "age": 34,
         "country": "India",
         "category": "All Rounder"
